cb613b192e9a254ad3ae3eed67b075d62eb30c91
Alex Sutay <ads1429@rit.edu>
2020-04-10 15:16:06 -0400

    Updated docstrings

--- ClassifierBot.py
+++ ClassifierBot.py
@@ -1,6 +1,6 @@
 """
+This is a discord bot designed to use predictImage.py to predict the class of an image sent to it
 author: Alex Sutay
-file: betbot.py
 """
 
 import discord
@@ -8,12 +8,23 @@ import predictImage
 import pic_to_vect
 from PIL import Image
 from config import TOKEN, THETA_FILE, CHANNEL, KEY
+"""
+TOKEN is the bot token provided by discord
+THETA_FILE is the name of the file containing the parameters for the neural network
+CHANNEL is the color channel you want to use. It can be 'r', 'g', 'b', or 'l'(grayscale)
+KEY is the answer key for what each number output represents. It should be a dictionary of integers to strings
+"""
 
 client = discord.Client()
 
 
 @client.event
 async def on_message(message):
+    """
+    Called once it receives a message
+    :param message: a discord message object
+    :return: None
+    """
     for attachment in message.attachments:
         await message.channel.send("Hmm, let me think for a moment...")
         await attachment.save("image")
